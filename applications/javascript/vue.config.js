b97bda5857b184dec7ce7b9ccef025ce4fd65582
rocco <rocco.mormont@gmail.com>
2019-07-19 08:25:11 +0800

    build(sub): 修改构建脚本

--- applications/javascript/vue.config.js
+++ applications/javascript/vue.config.js
@@ -11,13 +11,17 @@ module.exports = {
 
   productionSourceMap: false,
 
+  css: {
+    extract: false
+  },
+
   configureWebpack: {
     entry: './src/entry.js',
 
-    output: {
-      libraryExport: 'default',
-      jsonpFunction: `webpackJsonp-${APP_NAME}`
-    },
+    // output: {
+    //   libraryExport: 'default',
+    //   jsonpFunction: `webpackJsonp-${APP_NAME}`
+    // },
 
     externals: {
       vue: 'Vue',
@@ -30,6 +34,25 @@ module.exports = {
         'process.env.VUE_APP_NAME': JSON.stringify(APP_NAME)
       })
     ]
+  },
+
+  chainWebpack: (config) => {
+    if (NODE_ENV === 'development') {
+      config.output
+        .filename('main.js')
+        .chunkFilename('[name].[chunkhash:8].js')
+        .jsonpFunction(`webpackJsonp-${APP_NAME}`)
+        .library(APP_NAME)
+        // .libraryExport('default')
+        .libraryTarget('umd')
+
+      config.optimization.splitChunks(false)
+
+      config.plugins
+        .delete('html')
+        .delete('preload')
+        .delete('prefetch')
+    }
   },
 
   devServer: {
