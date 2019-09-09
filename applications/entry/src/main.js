0b89a8976bca68c23ed8d2b53150672b8823950c
rocco <rocco.mormont@gmail.com>
2019-09-09 18:18:16 +0800

    refactor: 通过配置注册子项目

--- applications/entry/src/main.js
+++ applications/entry/src/main.js
@@ -1,6 +1,8 @@
 import * as singleSpa from 'single-spa'
 import modules from './modules'
 
+window.NProgress.configure({ showSpinner: true })
+
 const { registerApplication, start, navigateToUrl } = singleSpa
 const cache = Object.create(null)
 
@@ -38,20 +40,41 @@ const loadApplication = async () => {
   return application
 }
 
-start() // 启动 single-spa
+const navigateToApplication = async (name) => {
+  console.log('navigateToApplication', name)
 
-window.NProgress.configure({ showSpinner: true })
-window.navigateToUrl = navigateToUrl
+  // if (modules.includes(name) && !cache[name]) {
+  //   console.log('await load application', name)
+  //   await window.System.import(`/${name}/main.js`)
+  // }
+
+  console.log('after load application', name)
+
+  navigateToUrl(`/#/${name}`)
+}
+
+const routes = [
+  {
+    path: '/javascript',
+    name: 'javascript',
+    application: () => loadApplication('javascript')
+  },
+  {
+    path: '/typescript',
+    name: 'typescript',
+    application: () => loadApplication('typescript')
+  }
+]
+
+const startSingleSpa = (routes = []) => {
+  start() // 启动 single-spa
+
+  routes.forEach(({ path, name, application }) => {
+    registerApplication(name, application, pathPrefix(path))
+  })
+}
+
+startSingleSpa(routes)
+
+window.navigateToApplication = navigateToApplication
 window._applications = cache
-
-registerApplication(
-  'javascript',
-  () => loadApplication('javascript'),
-  pathPrefix('/javascript')
-)
-
-registerApplication(
-  'typescript',
-  () => loadApplication('typescript'),
-  pathPrefix('/typescript')
-)
