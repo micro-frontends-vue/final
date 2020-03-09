27096c62713fb02eeba7d31d3bb75166d0b043c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-09 12:21:17 +0000

    Deploy micro-frontends-vue/final to github.com/micro-frontends-vue/final.git:gh-pages

--- typescript/about.14cfda64.js
+++ typescript/about.14cfda64.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp-typescript"]=window["webpackJsonp-typescript"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],c=n("2be6"),i={},r=Object(c["a"])(i,s,a,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=about.c663a1dc.js.map
+//# sourceMappingURL=about.14cfda64.js.map
