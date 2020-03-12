e34409eefde5376b9d939cb32fbaae89722f3684
rocco <rocco.mormont@gmail.com>
2020-03-12 12:10:02 +0800

    feat: 添加 Vue 版本展示

--- applications/javascript/src/App.vue
+++ applications/javascript/src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div class="javascript-app">
-    <div class="javascript-nav">
-      <router-link to="/javascript/home">Home</router-link> |
+  <div class="app">
+    <div class="nav">
+      <router-link to="/javascript/home">Home</router-link>
+      <span> | </span>
       <router-link to="/javascript/about">About</router-link>
     </div>
-    <router-view/>
+
+    <p class="versions">
+      <span>Vue: {{ vueVersion }}</span>
+      <span>VueRouter: {{ vueRouterVersion }}</span>
+      <span>Vuex: {{ vuexVersion }}</span>
+    </p>
+
+    <router-view />
   </div>
 </template>
 
-<style>
-.javascript-app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<script>
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+
+export default {
+  data() {
+    return {
+      vueVersion: Vue.version,
+      vueRouterVersion: VueRouter.version,
+      vuexVersion: Vuex.version
+    };
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-.javascript-nav {
+.nav {
   padding: 30px;
 }
 
-.javascript-nav a {
+.nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-.javascript-nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.versions > span {
+  font-size: 14px;
+  color: #999;
+}
+
+.versions > span + span {
+  margin-left: 10px;
 }
 </style>
--- applications/typescript/src/App.vue
+++ applications/typescript/src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div class="typescript">
-    <div class="typescript-nav">
-      <router-link to="/typescript/home">Home</router-link> |
+  <div class="app">
+    <div class="nav">
+      <router-link to="/typescript/home">Home</router-link>
+      <span> | </span>
       <router-link to="/typescript/about">About</router-link>
     </div>
-    <router-view/>
+
+    <p class="versions">
+      <span>Vue: {{ vueVersion }}</span>
+      <span>VueRouter: {{ vueRouterVersion }}</span>
+      <span>Vuex: {{ vuexVersion }}</span>
+    </p>
+
+    <router-view />
   </div>
 </template>
 
-<style>
-.typescript {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<script>
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+
+export default {
+  data() {
+    return {
+      vueVersion: Vue.version,
+      vueRouterVersion: VueRouter.version,
+      vuexVersion: Vuex.version
+    };
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-.typescript-nav {
+.nav {
   padding: 30px;
 }
 
-.typescript-nav a {
+.nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-.typescript-nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.versions > span {
+  font-size: 14px;
+  color: #999;
+}
+
+.versions > span + span {
+  margin-left: 10px;
 }
 </style>
